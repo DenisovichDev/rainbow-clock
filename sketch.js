92d4282f88d8e870c23bd77c3bd5b3e300c46a20
Bhaswar Chakraborty <66998096+Ivan-Denisovich-py@users.noreply.github.com>
2020-08-23 00:28:54 +0530

    Adjusted stuff in sketch.js

--- sketch.js
+++ sketch.js
@@ -11,10 +11,10 @@ var m;
 var n;
 var sec;
 //rect variables
-var l_rect=130;
-var b_rect=40;
-var xpoint=300;
-var ypoint=240;
+var l_rect = 130;
+var b_rect = 40;
+var xpoint = 280;
+var ypoint = 240;
 var colon_w = 60;
 //HTML objects
 var sliders;
@@ -26,14 +26,15 @@ var c2;
 var c3;
 var c4;
 var c5;
+var clock_color;
 
 function setup ()
 {
   canvas = createCanvas(1600*scaleFactor, 1000*scaleFactor);
-  
+
   sliders = makeSliders(colors);
   setSlider(sliders);
-  
+
   frameRate(1);
 
 }
@@ -41,13 +42,14 @@ function setup ()
 
 function draw(){
   background(255);
-  
+
   c1 = color(sliders[0][0].value(), sliders[0][1].value(), sliders[0][2].value());
   c2 = color(sliders[1][0].value(), sliders[1][1].value(), sliders[1][2].value());
   c3 = color(sliders[2][0].value(), sliders[2][1].value(), sliders[2][2].value());
   c4 = color(sliders[3][0].value(), sliders[3][1].value(), sliders[3][2].value());
   c5 = color(sliders[4][0].value(), sliders[4][1].value(), sliders[4][2].value());
-  
+  clock_color = sliders[5][0].value();
+
   h = int(hour()/10);
   r = hour()%10;
   m = int(minute()/10);
@@ -76,15 +78,15 @@ function draw(){
   seven_seg(nums[m], xpoint + 2*(180+l_rect)+70, ypoint, l_rect, b_rect);
   seven_seg(nums[n], xpoint + 3*(180+l_rect)+70, ypoint, l_rect, b_rect);
   if(frameCount % 2 == 0)
-  fill(255);
+  fill(clock_color);
   else
-  fill(255, 20);
+  fill(clock_color, 50);
   rect(xpoint + 1*(180+l_rect)+180, ypoint + 100, colon_w, colon_w, 50);
   rect(xpoint + 1*(180+l_rect)+180, ypoint + 240, colon_w, colon_w, 50);
 
   //Calendar
 
-   
+
 
   for(var i = 0; i < 3; i++){
       sixteen_seg(months[mnth][i], xCal, yCal, lCal, bCal);  // x : y : 7 : 2
@@ -109,11 +111,9 @@ function draw(){
 function getColor(value, shift)
 {//bitwise operation for alpha=0 or 1
   let alpha=255*(value>>shift & 1);
-  if(alpha==0)
-  alpha = 30;
-  let g = 255;
-  let b = 255;
-  let r = 255;
-  return color(r, g, b, alpha);
+  if(alpha == 0)
+  alpha = 50;
+
+  return color(clock_color, alpha);
 
 }
