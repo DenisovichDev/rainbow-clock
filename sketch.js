ceb4306184497d9959d2299be3b0035804f440f4
Bhaswar Chakraborty <66998096+Ivan-Denisovich-py@users.noreply.github.com>
2020-08-24 21:49:44 +0530

    blah blah blah

--- sketch.js
+++ sketch.js
@@ -27,21 +27,22 @@ var c3;
 var c4;
 var c5;
 var clock_color;
+let l;
 
-function setup ()
-{
+function setup (){
   canvas = createCanvas(1600*scaleFactor, 1000*scaleFactor);
 
   sliders = makeSliders(colors);
   setSlider(sliders);
 
   frameRate(1);
-
 }
 
 
 function draw(){
   background(255);
+  colorMode(RGB);
+
 
   c1 = color(sliders[0][0].value(), sliders[0][1].value(), sliders[0][2].value());
   c2 = color(sliders[1][0].value(), sliders[1][1].value(), sliders[1][2].value());
@@ -49,6 +50,7 @@ function draw(){
   c4 = color(sliders[3][0].value(), sliders[3][1].value(), sliders[3][2].value());
   c5 = color(sliders[4][0].value(), sliders[4][1].value(), sliders[4][2].value());
   clock_color = sliders[5][0].value();
+
 
   h = int(hour()/10);
   r = hour()%10;
@@ -64,10 +66,12 @@ function draw(){
   var yCal = ypoint + 2*l_rect + 3*b_rect + 70;
   var lCal = l_rect/6;  var bCal = 5;    // 7 : 2
 
+
   circularGrad(0, 0, c1, c5, 0.75);
   circularGrad(width, 0, c2, c5, 0.75);
   circularGrad(0, height, c3, c5, 0.75);
   circularGrad(width, height, c4, c5, 0.75);
+
 
 
   scale(scaleFactor);
@@ -77,10 +81,19 @@ function draw(){
   seven_seg(nums[r], xpoint + 1*(180+l_rect)-100, ypoint, l_rect, b_rect);
   seven_seg(nums[m], xpoint + 2*(180+l_rect)+70, ypoint, l_rect, b_rect);
   seven_seg(nums[n], xpoint + 3*(180+l_rect)+70, ypoint, l_rect, b_rect);
+
+  colorMode(HSL, 255);
+  l = 100;
+  if (clock_color >= 245) l = 255;
+  else if (clock_color <= 10) l = 0;
+
   if(frameCount % 2 == 0)
-  fill(clock_color);
+  fill(clock_color, 255, l);
   else
-  fill(clock_color, 50);
+  fill(clock_color, 255, l, 50);
+
+
+
   rect(xpoint + 1*(180+l_rect)+180, ypoint + 100, colon_w, colon_w, 50);
   rect(xpoint + 1*(180+l_rect)+180, ypoint + 240, colon_w, colon_w, 50);
 
@@ -108,12 +121,23 @@ function draw(){
   }
 
 
-function getColor(value, shift)
-{//bitwise operation for alpha=0 or 1
+function getColor(value, shift){
+  //bitwise operation for alpha=0 or 1
+
+  colorMode(HSL, 255);
+
+
   let alpha=255*(value>>shift & 1);
+  l = 100;
+
   if(alpha == 0)
   alpha = 50;
+  if(clock_color >= 245)
+  l = 255;
+  if(clock_color <= 10)
+  l = 0;
 
-  return color(clock_color, alpha);
+  return color(clock_color, 255, l, alpha);
+
 
 }
